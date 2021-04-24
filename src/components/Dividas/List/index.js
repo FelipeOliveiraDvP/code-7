71e24fcf0a89827647434a883c57727bf2564e17
Felipe de Oliveira <felipe.deoliveira@sp.senai.br>
2021-04-24 18:16:14 -0300

    Create dividas list

--- src/components/Dividas/List/index.js
+++ src/components/Dividas/List/index.js
@@ -1,8 +1,63 @@
 import React from "react";
-import { Table } from "antd";
+import { Button, Space, Table } from "antd";
+import moment from "moment";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
-function List() {
-  return <h1>List</h1>;
+import formatCurrency from "../../../utils/formatCurrency";
+
+function List({ dividas, loading, onEdit, onDelete }) {
+  const columns = [
+    {
+      title: "Motivo",
+      dataIndex: "motivo",
+      key: "motivo",
+    },
+    {
+      title: "Valor",
+      dataIndex: "valor",
+      key: "valor",
+      render: (text) => formatCurrency(text),
+    },
+    {
+      title: "Data da criação",
+      dataIndex: "criado",
+      key: "criado",
+      render: (text) => moment(text).format("DD/MM/YYYY"),
+    },
+    {
+      title: "Ações",
+      dataIndex: "acoes",
+      key: "acoes",
+      render: (text, record) => (
+        <Space>
+          <Button
+            type="primary"
+            icon={<EditOutlined />}
+            onClick={() => onEdit(record._id)}
+          >
+            Editar
+          </Button>
+          <Button
+            type="primary"
+            icon={<DeleteOutlined />}
+            danger
+            onClick={() => onDelete(record._id)}
+          >
+            Excluir
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={dividas}
+      rowKey={(row) => row._id}
+      loading={loading}
+    />
+  );
 }
 
 export default List;
--- src/pages/Dividas/index.js
+++ src/pages/Dividas/index.js
@@ -1,7 +1,53 @@
-import React from "react";
+import { notification } from "antd";
+import React, { useEffect, useState } from "react";
+
+import List from "../../components/Dividas/List";
+import { getDividas } from "../../services/dividas";
 
 function Dividas() {
-  return <h1>Dividas</h1>;
+  const [dividas, setDividas] = useState([]);
+  const [divida, setDivida] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [visible, setVisible] = useState(false);
+
+  const fetch = () => {
+    setLoading(true);
+    getDividas()
+      .then(({ data }) => {
+        const { result } = data;
+        setDividas(result);
+      })
+      .catch(({ response }) => {
+        notification.error({
+          description: "Erro ao listar as dívidas",
+        });
+      })
+      .finally(() => setLoading(false));
+  };
+
+  const handleEdit = (id) => {
+    console.log(id);
+  };
+
+  const handleDelete = (id) => {
+    console.log(id);
+  };
+
+  useEffect(() => {
+    fetch();
+  }, [divida]);
+
+  return (
+    <>
+      <h2>Dívidas</h2>
+      <List
+        dividas={dividas}
+        loading={loading}
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+      />
+    </>
+  );
 }
 
 export default Dividas;
